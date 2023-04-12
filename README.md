002cd56f07425985954ca081280c3f91f29e1af2
john russelle domingo <johnrusselledomingo@gmail.com>
2023-04-12 16:54:11 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# spring-guides-consuming-rest
+
+
+
+ follow this link
+link: https://spring.io/guides/gs/consuming-rest/
+
+reference: https://github.com/kiddogreed/spring-guides-consuming-rest
+
---
+++ src/main/java/com/springguides/consumingrest/ConsumingRestApplication.java
@@ -0,0 +1,14 @@
+package com.springguides.consumingrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumingRestApplication {
+
+	public static void main(String[] args) {
+		System.out.println(123);
+		SpringApplication.run(ConsumingRestApplication.class, args);
+	}
+
+}
